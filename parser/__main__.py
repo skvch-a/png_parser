0382d01c2cbcc6ccde594f2de3df3ba3818ed2ea
Aleksey <skvch.al@gmail.com>
2024-12-13 02:13:37 +0500

    refactoring

--- parser/__main__.py
+++ parser/__main__.py
@@ -3,50 +3,47 @@ from PIL import Image
 import os
 
 
-def read_png_headers(file_path):
+def parse_file(file_path):
     headers = []
     try:
         with open(file_path, 'rb') as file:
             signature = file.read(8)
             if signature != b'\x89PNG\r\n\x1a\n':
-                raise ValueError("Это не PNG файл.")
+                raise ValueError("это не PNG файл")
 
             while True:
-                length_data = file.read(4)
-                if len(length_data) < 4:
-                    break
-                length = struct.unpack('>I', length_data)[0]
-
-                chunk_type = file.read(4).decode('ascii')
-                data = file.read(length)
-
-                crc_data = file.read(4)
-                crc = struct.unpack('>I', crc_data)[0]
-
-                # Добавляем информацию о заголовке
-                headers.append({
-                    'Chunk Type': chunk_type,
-                    'Length': length,
-                    'CRC': crc,
-                    'Data Size': len(data),
-                    'Data': data
-                })
-
-                if chunk_type == 'IEND':
+                header = parse_header(file)
+                headers.append(header)
+                if header['Chunk Type'] == 'IEND':
                     break
     except Exception as e:
         print(f"Ошибка при чтении файла: {e}")
     return headers
 
 
+def parse_header(file):
+    length = struct.unpack('>I', file.read(4))[0]
+    chunk_type = file.read(4).decode('ascii')
+    data = file.read(length)
+    crc = struct.unpack('>I', file.read(4))[0]
+    return {
+        'Chunk Type': chunk_type,
+        'Length': length,
+        'CRC': crc,
+        'Data Size': len(data),
+        'Data': data
+    }
+
+
 def print_headers(headers):
+    print('-' * 50)
     print(f"{'Chunk Type':<12} {'Length':<8} {'CRC':<12} {'Data Size':<12}")
     print("-" * 50)
     for header in headers:
         print(f"{header['Chunk Type']:<12} {header['Length']:<8} {header['CRC']:<12} {header['Data Size']:<12}")
 
 
-def decode_IHDR(data):
+def decode_ihdr(data):
     width, height = struct.unpack('>II', data[:8])
     bit_depth = data[8]
     color_type = data[9]
@@ -64,39 +61,32 @@ def decode_IHDR(data):
     print(f"Глубина цвета: {bit_depth}")
     print(f"Тип цвета: {color_types.get(color_type, 'Неизвестный тип')}")
 
-def decode_PLTE(data):
-    print(f"Размер палитры: {len(data) // 3} цветов")
 
-def show_png_image(file_path):
-    try:
-        image = Image.open(file_path)
-        image.show()  # Отображаем изображение с использованием PIL
-    except Exception as e:
-        print(f"Ошибка при отображении изображения: {e}")
+def decode_plte(data):
+    print(f"Размер палитры: {len(data) // 3} цветов")
 
 
 def main():
     file_path = input("Введите путь к файлу: ").strip()
-
+    print('-' * 50)
     file_name = os.path.basename(file_path)
     file_size = os.path.getsize(file_path)
-    print(f"\nИмя файла: {file_name}")
+
+    print(f"Имя файла: {file_name}")
     print(f"Размер файла: {file_size} байт")
-
-    headers = read_png_headers(file_path)
+    print('-' * 50)
+    headers = parse_file(file_path)
     if headers:
-        print("Заголовки PNG файла:")
-        print_headers(headers)
-
         for header in headers:
             chunk_type = header['Chunk Type']
             data = header['Data']
             if chunk_type == 'IHDR':
-                decode_IHDR(data)
+                decode_ihdr(data)
             elif chunk_type == 'PLTE':
-                decode_PLTE(data)
+                decode_plte(data)
 
-        show_png_image(file_path)
+        print_headers(headers)
+        Image.open(file_path).show()
 
 
 if __name__ == '__main__':
